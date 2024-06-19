ab736371d3ae06b903a208ded1bf5c2970f7132e
KazuakiShigefuji <shigefuji@gmail.com>
2024-06-19 09:15:15 +0900

    微修正

--- Post2BSky.py
+++ Post2BSky.py
@@ -169,12 +169,11 @@ class BlueSkyBot:
             try:
                 japanese_quote_message = self.generate_description(self.fetch_japanese_quote(), "ポジティブな言葉で元気が出る解説をしてください。質問の言葉を解答から除いてください")
             except Exception as e:
-                pass  
+                print("Retry:Generate description\n")
             else:
                 break 
         else:
             japanese_quote_message = "今日は何もない日ですが、特別な一日です。元気を出して行きましょう！"
-            pass
 
         for message in [weather_message, exchange_rate_message, english_quote_message, trends_message, japanese_quote_message]:
             print("Postlog No:", self.log_to_gspread(timestamp, message), "\n")
